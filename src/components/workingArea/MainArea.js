da38b13d30a0e2011d15ea3f78fe4fe3daf5bb64
surya5602 <suryakgm1234@gmail.com>
2022-10-27 14:24:20 +0530

    recent updates

--- src/components/workingArea/MainArea.js
+++ src/components/workingArea/MainArea.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SearchBar from '../searchBar/SearchBar'
 import ActiveIcon from '../ActiveIcon/ActiveIcon'
+import Icons from '../Icons/Icons'
 import "./mainarea.css"
 function MainArea() {
   return (
     <div className='main-area flex'>
           <SearchBar/>
           <ActiveIcon/>
+          <Icons/>
     </div>
    
   )
